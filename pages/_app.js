bf1b6528e8c83e41bb1ef9df0e0ecf1ac7a4fe2e
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-10-26 18:35:00 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/global.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <React.Fragment>
+  <Router>
+    <NavigationBar />
+	<Switch>
+	return <Component {...pageProps} />
+	</Switch>
+  </Router>
+</React.Fragment>
 }
